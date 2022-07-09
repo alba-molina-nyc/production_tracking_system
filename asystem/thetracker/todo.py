adff423bfc6f0d088f11f4a1d7743a6f90851371
Username <albavmolinanyc@gmail.com>
2022-07-09 18:26:49 -0400

    uses bootstrap to fix the table of jobs a bit

--- asystem/thetracker/todo.py
+++ asystem/thetracker/todo.py
@@ -1,16 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-import datetime
-
-# Create your models here.
-
-# TODO: go to : asystem/thetracker/models.py and change setter_name to on_delete=models.SET(and send to a function that saves this item we are deleting to the admin user)
-from django.db import models
-from django.contrib.auth.models import User
 from django.urls import reverse
 
 # Create your models here.
-
+# TODO: go to : asystem/thetracker/models.py and change setter_name to on_delete=models.SET(and send to a function that saves this item we are deleting to the admin user)
 class Job(models.Model):
     order_num = models.CharField(max_length=275)
     SKU = models.CharField(max_length=275)
@@ -24,12 +17,13 @@ class Job(models.Model):
     def __str__(self):
         return self.order_num + ' | ' + str(self.setter_name) + ' | ' + str(self.created)
     
+    #TODO: gauge which return he wants
     def get_absolute_url(self):
         return reverse('add-job')
-        # or
-        return reverse('home') # to send back to home
-        # or
-        return reverse('job-detail', args=(str(self.id)))
+        # or -> to return to home and avoid passing in self.id 
+        # return reverse('home') # to send back to home
+        # or -> to return into the new job the user just created
+        # return reverse('job-detail', args=(str(self.id)))
 
        
 
