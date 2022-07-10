e1300b1fc7a8ee8300093d36dfba3472985a3c16
Username <albavmolinanyc@gmail.com>
2022-07-09 20:04:10 -0400

    it is a 200 method but not populating

--- asystem/thetracker/urls.py
+++ asystem/thetracker/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-# from . import views
-from .views import HomeView, JobDetailView, AddJobView
+from . import views
+from .views import HomeView, JobDetailView, AddJobView, filterView
 
 urlpatterns = [
     # path('', views.home, name='home'),
     path('', HomeView.as_view(), name='home'),
+    path('', filterView, name='filterView'),
     path('job/<int:pk>', JobDetailView.as_view(), name='job-detail'),
     path('add_job', AddJobView.as_view(), name='add-job'),
 ]
--- asystem/thetracker/views.py
+++ asystem/thetracker/views.py
@@ -1,16 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView
+from rest_framework import generics
+from rest_framework.response import Response
 from .models import Job
+from .serializers import JournalSerializer
+
 
 # Create your views here.
 
 # def home(request):
 #     return render(request, 'home.html', {})
 
+def is_valid_queryparam(param):
+    return param != '' and param is not None
+
+def filter(request):
+    qs = Job.objects.all()
+    order_num = request.GET.get('order_num')
+    SKU = request.GET.get('SKU')
+    setter_name = request.GET.get('setter_name')
+    created = request.GET.get('created')
+
+    if is_valid_queryparam(order_num):
+        qs = qs.filter(order_num = order_num)
+    
+    if is_valid_queryparam(SKU):
+        qs = qs.filter(SKU = SKU )
+    
+    if is_valid_queryparam(setter_name):
+        qs = qs.filter(setter_name = setter_name)
+
+    if is_valid_queryparam(created):
+        qs = qs.filter(created = created)
+
+
+    return qs
+
+
+
+
+def filterView(request):
+    qs = filter(request)
+    context = {
+        'queryset': qs,
+        'jobs': Job.objects.all()}
+    return render(request, 'home.html', context)
 
 class HomeView(ListView):
     model = Job
     template_name = 'home.html'
+
+
+
+
 
 class JobDetailView(DetailView):
     model = Job
