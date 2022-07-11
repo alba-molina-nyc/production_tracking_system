db761370cd705003e706ae7d4cf3b58064ebd0d7
Username <albavmolinanyc@gmail.com>
2022-07-11 13:22:39 -0400

    implementing jobfilter by icontains while commenting out exact filtering

--- asystem/thetracker/filters.py
+++ asystem/thetracker/filters.py
@@ -1,8 +1,24 @@
 import django_filters
-from .models import *
+from .models import Job
 
-# build python class that is going to build filter for us
+# this version uses a dictionary to match items that "contain"
 class JobFilter(django_filters.FilterSet):
     class Meta:
         model = Job
-        fields = '__all__'
+        fields = {
+            'order_num' : ['icontains'],
+            'SKU' : ['icontains'],
+            'num_items' : ['icontains'],
+            'num_stones' : ['icontains'],
+            'description_text' : ['icontains'],
+            'created' : ['icontains'],
+            'updated' : ['icontains'],
+        }
+
+
+
+# build python class that is going to build filter - this version only shows exact search
+# class JobFilter(django_filters.FilterSet):
+#     class Meta:
+#         model = Job
+#         fields = '__all__'
