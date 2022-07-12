72a7b4f81bb7d51533f7049ac8293b922ebdba5f
Username <albavmolinanyc@gmail.com>
2022-07-12 09:57:42 -0400

    change model from "setter" to "created by"

--- asystem/thetracker/models.py
+++ asystem/thetracker/models.py
@@ -10,12 +10,12 @@ class Job(models.Model):
     num_items = models.IntegerField()
     num_stones = models.IntegerField()
     description_text = models.TextField()
-    setter_name = models.ForeignKey(User, on_delete=models.PROTECT)
+    creator_name = models.ForeignKey(User, on_delete=models.PROTECT)
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
 
     def __str__(self):
-        return self.order_num + ' | ' + str(self.setter_name) + ' | ' + str(self.created)
+        return self.order_num + ' | ' + str(self.creator_name) + ' | ' + str(self.created)
     
     def get_absolute_url(self):
         return reverse('add-job')
